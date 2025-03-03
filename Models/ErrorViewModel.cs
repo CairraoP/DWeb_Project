be020733adfa23274e349a70d1b3e63627ecbb9d
Paulo Pereira <tisppereira@gmail.com>
2025-03-03 16:19:04 +0000

    Import existing project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Final_Project_Mockup.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
